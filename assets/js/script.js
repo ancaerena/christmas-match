7e00193d2ce772cf6dc470307afb6cbe5f2388ae
ancaerena <anca.erena@yahoo.ro>
2022-10-04 15:59:03 +0000

    Add functions for when cards match

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,43 @@ function flipCard() {
    if (!hasFlippedCard) {
      hasFlippedCard = true;
      firstCard = this;
-   }
+     return;
+  }
 
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
 }
+// *functions to keep cards flipped when a match is found
+// *code source for checkForMatch, disable cards and unflip cards: https://marina-ferreira.github.io/
+
+
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
+}
+
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-// function to keep cards flipped when a match is found
-function checkForMarch() {
 
-}
+
 // function to reset the board game
 function resetGame() {
 
