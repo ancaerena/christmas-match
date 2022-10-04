0a5c11df72545806f7b48b0c17e7f65e98ce9766
ancaerena <anca.erena@yahoo.ro>
2022-10-04 15:21:27 +0000

    Add content to flipCard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
 // function to record the number of clicks and to flip the cards
+const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
+    this.classList.add('flip');
+
+   if (!hasFlippedCard) {
+     hasFlippedCard = true;
+     firstCard = this;
+   }
 
 }
+cards.forEach(card => card.addEventListener('click', flipCard));
+
 // function to keep cards flipped when a match is found
 function checkForMarch() {
 
