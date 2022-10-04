1318db20c436bc51a3f0efcf0668a48d9f42548d
ancaerena <anca.erena@yahoo.ro>
2022-10-04 14:58:11 +0000

    Add style to timer and moves div and add function structure

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// function to record the number of clicks and to flip the cards
+function flipCard() {
+
+}
+// function to keep cards flipped when a match is found
+function checkForMarch() {
+
+}
+// function to reset the board game
+function resetGame() {
+
+}
+
+// function to shuffle the cards
+ function suffleCards() {
+
+ }
+
