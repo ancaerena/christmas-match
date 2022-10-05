caeaf07491f61839e5b8a134112ebfd4ebb8175d
ancaerena <anca.erena@yahoo.ro>
2022-10-05 16:12:52 +0000

    Add shuffle and returnboard functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // function to record the number of clicks and to flip the cards
 const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
     this.classList.add('flip');
 
    if (!hasFlippedCard) {
@@ -13,7 +16,6 @@ function flipCard() {
   }
 
   secondCard = this;
-  hasFlippedCard = false;
 
   checkForMatch();
 }
@@ -33,26 +35,39 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetGame();
 }
 
 function unflipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+    resetGame();
   }, 1500);
+
 }
+
+// function to reset the board game
+function resetGame() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 
 
-// function to reset the board game
-function resetGame() {
-
-}
 
 // function to shuffle the cards
- function suffleCards() {
+(function shuffleCards() {
+  cards.forEach(card => {
+    let ramdomPos = Math.floor(Math.random() * 16);
+    card.style.order = ramdomPos;
+  });
+})();
 
- }
 
