15b5e024063a081aa09e9f3a4476294e10582343
ancaerena <anca.erena@yahoo.ro>
2023-05-06 13:39:33 +0000

    Fetch json file in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,45 @@ let lockBoard = false;
 let score = 0;
 
 document.querySelector(".score").textContent = score;
+
+// pull the cards from json file using the Fetch API method
+fetch("./public/cards.json")
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    generateCards();
+  });
+
+// shuffle the cards
+
+  function shuffleCards() {
+   let currentIndex = cards.length,
+     randomIndex,
+     temporaryValue;
+   while (currentIndex !== 0) {
+     randomIndex = Math.floor(Math.random() * currentIndex);
+     currentIndex -= 1;
+     temporaryValue = cards[currentIndex];
+     cards[currentIndex] = cards[randomIndex];
+     cards[randomIndex] = temporaryValue;
+   }
+  }
+
+  // generate the cards 
+
+   function generateCards() {
+      for (let card of cards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
+        cardElement.innerHTML = `
+          <div class="front">
+            <img class="front-image" src=${card.image} />
+          </div>
+          <div class="back"></div>
+        `;
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
+      }
+    }
